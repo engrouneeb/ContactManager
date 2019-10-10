c58136a455719b7d089ab3d69cd801d6073cc486
Ouneeb Ur Rehman <shafiullah@Shafis-MacBook-Pro.local>
2019-10-11 01:27:17 +0500

    Updates

---
+++ precache-manifest.1ab49131462f90a300001084e6a3597c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a29efa2144d9086ea2d72913983b22",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "1311d77ca63b7205cf08",
+    "url": "/ContactManager/static/css/2.9ef9e05a.chunk.css"
+  },
+  {
+    "revision": "f1a9187911e5398edada",
+    "url": "/ContactManager/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1311d77ca63b7205cf08",
+    "url": "/ContactManager/static/js/2.49c562d7.chunk.js"
+  },
+  {
+    "revision": "f1a9187911e5398edada",
+    "url": "/ContactManager/static/js/main.0eff4295.chunk.js"
+  },
+  {
+    "revision": "70069e46b9c690bcfeed",
+    "url": "/ContactManager/static/js/runtime-main.4eb5b3a6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ContactManager/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ContactManager/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ContactManager/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ContactManager/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ContactManager/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
